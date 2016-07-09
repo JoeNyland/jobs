bd91624be909d0c26bba8169c465f02ee380fb4b
Joe Nyland <joenyland@me.com>
2016-07-09 15:06:39 +0100

    Complete test case 1

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,5 +1,13 @@
 require "jobs/version"
 
 module Jobs
-  # Your code goes here...
+  def self.order(job_struct)
+
+    # Make sure we've been provided with the correct parameter type
+    raise ArgumentError, '' unless job_struct.is_a? Hash or String
+
+    # If we've not been provided with jobs to run, then return an empty sequence
+    return '' if job_struct.is_a? String and job_struct.empty?
+
+  end
 end
