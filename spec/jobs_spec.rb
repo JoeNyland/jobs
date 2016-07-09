51ab0353a862e942112c411cddc3ac9e389145a3
Joe Nyland <joenyland@me.com>
2016-07-09 14:58:21 +0100

    Complete writing tests

--- spec/jobs_spec.rb
+++ spec/jobs_spec.rb
@@ -9,26 +9,50 @@ describe Jobs do
     describe 'Jobs#order' do
 
       # Given you’re passed an empty string (no jobs), the result should be an empty sequence.
-      it 'should pass example 1'
+      it 'should pass example 1' do
+        job_structure = ''
+        expected_output = ''
+        expect(Jobs.order(job_structure)).to eq expected_output
+      end
 
       # Given the following job structure:
       #   a =>
       # The result should be a sequence consisting of a single job a.
-      it 'should pass example 2'
+      it 'should pass example 2' do
+        job_structure = { a: nil }
+        expected_output = 'a'
+        expect(Jobs.order(job_structure)).to eq expected_output
+      end
 
       # Given the following job structure:
       #   a =>
       #   b =>
       #   c =>
       # The result should be a sequence containing all three jobs abc in no significant order.
-      it 'should pass example 3'
+      it 'should pass example 3' do
+        job_structure = {
+          a: nil,
+          b: nil,
+          c: nil
+        }
+        expected_output = 'abc'
+        expect(Jobs.order(job_structure)).to eq expected_output
+      end
 
       # Given the following job structure:
       #   a =>
       #   b => c
       #   c =>
       # The result should be a sequence that positions c before b, containing all three jobs abc.
-      it 'should pass example 4'
+      it 'should pass example 4' do
+        job_structure = {
+          a: nil,
+          b: :c,
+          c: nil
+        }
+        expected_output = 'acb'
+        expect(Jobs.order(job_structure)).to eq expected_output
+      end
 
       # Given the following job structure:
       #   a =>
@@ -39,14 +63,32 @@ describe Jobs do
       #   f =>
       # The result should be a sequence that positions f before c, c before b, b before e and a before d containing
       # all six jobs abcdef.
-      it 'should pass example 5'
+      it 'should pass example 5' do
+        job_structure = {
+          a: nil,
+          b: :c,
+          c: :f,
+          d: :a,
+          e: :b,
+          f: nil
+        }
+        expected_output = 'afcbde'
+        expect(Jobs.order(job_structure)).to eq expected_output
+      end
 
       # Given the following job structure:
       #   a =>
       #   b =>
       #   c => c
       # The result should be an error stating that jobs can’t depend on themselves.
-      it 'should pass example 6'
+      it 'should pass example 6' do
+        job_structure = {
+          a: nil,
+          b: nil,
+          c: :c
+        }
+        expect(Jobs.order(job_structure)).to raise_error(Jobs::DependencyError, "Jobs can't depend on themselves")
+      end
 
       # Given the following job structure:
       #   a =>
@@ -56,7 +98,18 @@ describe Jobs do
       #   e =>
       #   f => b
       # The result should be an error stating that jobs can’t have circular dependencies.
-      it 'should pass example 7'
+      it 'should pass example 7' do
+        job_structure = {
+          a: nil,
+          b: :c,
+          c: :f,
+          d: :a,
+          e: nil,
+          f: :b
+        }
+        expect(Jobs.order(job_structure)).to raise_error(Jobs::CircularDependencyError,
+                                                         "Jobs can't have circular dependencies")
+      end
 
     end
   end
