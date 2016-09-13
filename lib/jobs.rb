53a1e0bc86ae2673960fce7e40d40dde078c355b
Joe Nyland <joenyland@me.com>
2016-09-13 22:11:08 +0100

    Improve the handling of jobs with circular dependencies so that I don't need to rely on a SystemStackError

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -26,8 +26,7 @@ class Jobs
       unless dependent_job.nil?
         unless @sequence.include? dependent_job
           # This job has at least one dependency so we need to ensure it's dependent job(s) is/are executed first
-          dependencies = process_dependency(dependent_job)
-          @sequence.concat dependencies
+          @sequence.concat process_dependency(job, dependent_job)
         end
       end
 
@@ -44,28 +43,27 @@ class Jobs
 
   private
 
-  def process_dependency(dependent_job)
-    begin
-    if @job_struct[dependent_job].nil?
-      # No dependencies of it's own, so it can be run on it's own
-      [dependent_job]
-    else
-      # Dependent job has dependencies of it's own so we need to process them first
-      unless @job_struct[@job_struct[dependent_job]].nil?
-        process_dependency(@job_struct[dependent_job])
-      end
-      [@job_struct[dependent_job],dependent_job]
+  def process_dependency(job, dependency)
+
+    # Stop endless loop if there are circular dependencies and raise a suitable error
+    if job == dependency
+      raise CircularDependencyError, "Jobs can't have circular dependencies"
     end
-    rescue SystemStackError => e
-      # Check that the exception that we are rescuing is the correct one
-      if e.message == 'stack level too deep'
-        # Raise our own exception for this case
-        raise CircularDependencyError, "Jobs can't have circular dependencies"
-      else
-        # This isn't the exception were looking for, so re-raise
-        raise e
-      end
+
+    # No dependencies of it's own, so it can be run on it's own
+    return [dependency] if @job_struct[dependency].nil?
+
+    # Dependency has dependencies of it's own so we need to process them first
+    dependent_job = @job_struct[dependency]
+
+    unless @job_struct[dependent_job].nil?
+      # The dependency is dependent on another job
+      process_dependency job, @job_struct[dependent_job]
     end
+
+    # The dependency doesn't have any dependencies of it's own
+    [dependent_job,dependency]
+
   end
 
 end
